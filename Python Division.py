58affabb7c3df64682edbc8ee3b586f4b7e71619
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-06 00:44:40 +0000

    Update Python Division.py

---
+++ Python Division.py
@@ -0,0 +1,12 @@
+# Author: Nazmul Hassan
+# Submit: HackerRank
+# Date: 06/01/2021
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+# Hacker Rank Solution From Here
+
+print(a // b)
+print(a / b)
