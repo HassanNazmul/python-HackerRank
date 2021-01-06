d14815a17006745a22644e9e15d5e882ad2311a7
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-06 21:12:50 +0000

    Update Nested Lists.py

--- Nested Lists.py
+++ Nested Lists.py
@@ -2,5 +2,9 @@
 # Submit: HackerRank
 # Date: 06/01/2021
 
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
 
 # Hacker Rank Solution From Here
