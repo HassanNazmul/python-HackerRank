9150bd7160af398ea4459165d1ea7b9f38cf4446
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-06 00:23:02 +0000

    Update Python If-Else.py

--- Python If-Else.py
+++ Python If-Else.py
@@ -8,7 +8,14 @@ import random
 import re
 import sys
 
-n = int(input("Enter a Number: "))
+if __name__ == '__main__':
+    n = int(input().strip())
 
-if (n % 2) == 0:
-    print("{0} Weird".format(n))
+if n % 2 == 1:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
