387fcce4489a5f504b0d0275d15ee75f94edc688
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-06 00:35:24 +0000

    Update Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,13 @@
+# Author: Nazmul Hassan
+# Submit: HackerRank
+# Date: 06/01/2021
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+# Hacker Rank Solution From Here
+
+print(a + b)
+print(a - b)
+print(a * b)
