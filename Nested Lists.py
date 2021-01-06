1cfcb90235bfa705785aedf8dd5f08b166eaa533
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-06 22:58:42 +0000

    Update Nested Lists.py

--- Nested Lists.py
+++ Nested Lists.py
@@ -2,9 +2,20 @@
 # Submit: HackerRank
 # Date: 06/01/2021
 
+dic = []
+marks = []
+
 if __name__ == '__main__':
     for _ in range(int(input())):
         name = input()
         score = float(input())
 
 # Hacker Rank Solution From Here
+
+dic += [[name, score]]
+marks += [score]
+
+sl = (sorted(set(marks)))[1]
+for i, j in sorted(dic):
+    if j == sl:
+        print(i)
