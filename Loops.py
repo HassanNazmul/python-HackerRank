53d50acfe5cbe59d67867bf9a0ff03a6fbd88224
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-06 00:59:58 +0000

    Update Loops.py

---
+++ Loops.py
@@ -0,0 +1,13 @@
+# Author: Nazmul Hassan
+# Submit: HackerRank
+# Date: 06/01/2021
+
+if __name__ == '__main__':
+    n = int(input())
+
+# Hacker Rank Solution From Here
+
+i = 0
+while (i < n):
+    print(i * i)
+    i += 1
