bfef856c9e479d46b73e7e9ea5bc9803459b03ae
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-06 20:28:04 +0000

    Update List Comprehensions.py

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -2,5 +2,14 @@
 # Submit: HackerRank
 # Date: 06/01/2021
 
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
 
 # Hacker Rank Solution From Here
+
+res = [[i, j, k] for i in range(x+1) for j in range(y+1)
+       for k in range(z+1) if i+j+k != n]
+print(res)
