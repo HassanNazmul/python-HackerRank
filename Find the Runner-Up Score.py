70715d90e882d51e97bd8cf057c542046e2ab804
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-06 20:39:38 +0000

    Update Find the Runner-Up Score.py

--- Find the Runner-Up Score.py
+++ Find the Runner-Up Score.py
@@ -2,5 +2,11 @@
 # Submit: HackerRank
 # Date: 06/01/2021
 
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
 
 # Hacker Rank Solution From Here
+
+arr.sort()
+print(arr[(arr.index(max(arr)))-1])
