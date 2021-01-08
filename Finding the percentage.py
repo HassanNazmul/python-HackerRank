ac71242107b55f20c3949a7b2291f71d17e524f0
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-08 22:38:23 +0000

    Update Finding the percentage.py

--- Finding the percentage.py
+++ Finding the percentage.py
@@ -2,5 +2,17 @@
 # Submit: HackerRank
 # Date: 08/01/2021
 
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
 
 # Hacker Rank Solution From Here
+
+output = list(student_marks[query_name])
+per = sum(output)/len(output):
+print("%.2f" % per)
