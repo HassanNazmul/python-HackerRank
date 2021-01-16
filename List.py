ee70ba83a8d3eaf4de373e205c16f45d1b4e1acd
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-16 22:35:43 +0000

    Update List.py

--- List.py
+++ List.py
@@ -1,6 +1,11 @@
 # Author: Nazmul Hassan
 # Submit: HackerRank
-# Date: 06/01/2021
+# Date: 16/01/2021
 
+if __name__ == '__main__':
+    N = int(raw_input())
 
 # Hacker Rank Solution From Here
+
+list = []
+for i in range(N):
