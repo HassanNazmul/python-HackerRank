21953c6c114bfa6b7d1aa1c3bd2f2d7053cd8156
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-01-16 22:35:40 +0000

    Create Whats Your Name.py

---
+++ Whats Your Name.py
@@ -0,0 +1,11 @@
+# Author: Nazmul Hassan
+# Submit: HackerRank
+# Date: 16/01/2021
+
+def print_full_name(a, b):
+    print("")
+
+
+if __name__ == '__main__':
+
+    # Hacker Rank Solution From Here
