4f628b56cb1371736f18beb30451b466f70a0c38
Nazmul Hassan <nazmul.naaz96@gmail.com>
2021-01-20 11:27:33 +0000

    Update Find STRING.py

--- Find STRING.py
+++ Find STRING.py
@@ -3,8 +3,14 @@
 # Date: 20/01/2021
 
 def count_substring(string, sub_string):
-    return
+    # Hacker Rank Solution From Here
+
+    c=0
+    l=len(sub_string)
+    for i  in range(len(string)):
+        s=string[i:i+l]
+        if s==sub_string:
+            c+=1
+    return c
 
 if __name__ == '__main__':
-
-# Hacker Rank Solution From Here
