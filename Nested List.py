b425f1ea6debb628096e542a9f47bb405906d607
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-02-05 18:36:18 +0000

    Update Nested List.py

--- Nested List.py
+++ Nested List.py
@@ -2,25 +2,25 @@
 # Submit: HackerRank
 # Date: 05/02/2021
 
+Student = []
+Second_Lowest_Grade = []
+Grade = set()
+
 if __name__ == "__main__":
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        Student.append([name, score])
+        Grade.add(score)
 
 # Hacker Rank Solution From Here
 
-Student = []
-Second_Lowest_Grade = []
-Grade =set()
-
-Student.append([name, score])
-Grade.add(score)
 
 Second_Lowest = sorted(Grade)[1]
 
 for name, score in Student:
-    if score == Second_Lowest
-    Second_Lowest_Grade.append(name)
+    if score == Second_Lowest:
+        Second_Lowest_Grade.append(name)
 
 for name in sorted(Second_Lowest_Grade):
-    print(name, end='\n')
+    print(name, end="\n")
