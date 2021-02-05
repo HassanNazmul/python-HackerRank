00b356aa06f1e551ab2ed47864d0cd41551914e6
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-02-05 19:16:46 +0000

    Create Mutations.py

---
+++ Strings/Mutations.py
@@ -0,0 +1,16 @@
+# Author: Nazmul Hassan
+# Submit: HackerRank
+# Date: 05/02/2021
+
+# Hacker Rank Solution From Here
+
+
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position + 1 :]
+
+
+if __name__ == "__main__":
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
